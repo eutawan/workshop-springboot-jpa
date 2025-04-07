f5b6ab771c0a8b8a97706943cdcd2cc51025a173
eutawan <principalacc169@gmail.com>
2025-04-07 03:15:57 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/cursojava/course/CourseApplication.java
+++ src/main/java/com/cursojava/course/CourseApplication.java
@@ -1,13 +1,28 @@
 package com.cursojava.course;
 
+import com.cursojava.course.entities.User;
+import com.cursojava.course.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			userRepository.save(new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"));
+			userRepository.save(new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"));
+			System.out.println("Usuários inseridos!");
+		};
 	}
 
 }
