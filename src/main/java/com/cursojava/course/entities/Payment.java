a19b768f8dfb522e20b585e599719700f304f6a9
eutawan <principalacc169@gmail.com>
2025-04-13 19:22:27 -0300

    insertion payment association

--- src/main/java/com/cursojava/course/entities/Payment.java
+++ src/main/java/com/cursojava/course/entities/Payment.java
@@ -1,6 +1,7 @@
 package com.cursojava.course.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
